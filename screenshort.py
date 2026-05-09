cf5cfdc8a0b6f518fd720811e02bf4896c86501f
Abila Khan <khankeya961@gmail.com>
2026-05-10 03:27:11 +0600

    Create screenshort.py

---
+++ screenshort.py
@@ -0,0 +1,11 @@
+import pyautogui, time , os
+
+os.makedirs('screenshots', exist_ok=True)
+
+while True:
+    pyautogui.screenshot(f'screenshots/screenshort_{int(time.time())}.png')
+
+    print ('Screenshot taken!')
+    print ('Waiting for 5 seconds...')
+
+    time.sleep(10)
